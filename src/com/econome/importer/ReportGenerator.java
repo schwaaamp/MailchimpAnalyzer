5cb355da2e05f736c8a4c572f030d6bf9514554d
Erin Osterfeld <erin.osterfeld@gmail.com>
2021-10-29 16:23:45 -0400

    testing commit with username: email, password: git personal access token

--- src/com/econome/importer/ReportGenerator.java
+++ src/com/econome/importer/ReportGenerator.java
@@ -132,6 +132,7 @@ public class ReportGenerator {
 				}
 			}
 		}
+		//testing commit with personal access token
 		System.out.println("Finished determining the most recent open email timestamp by subscriber");
 	}
 
